799da1101ae36dfbbba0b42be8fb467683295280
TheNetWyvern <thenetwyvern@gmail.com>
2020-06-28 18:38:39 +0300

    Initial commit

---
+++ ToggleExplorer/ApplicationActivationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ToggleExplorer.Enums;
+using ToggleExplorer.Interfaces;
+
+namespace ToggleExplorer
+{
+    [ComImport, Guid("45BA127D-10A8-46EA-8AB7-56EA9078943C")]//Application Activation Manager
+    class ApplicationActivationManager : IApplicationActivationManager
+    {
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)/*, PreserveSig*/]
+        public extern IntPtr ActivateApplication([In] String appUserModelId, [In] String arguments, [In] ActivateOptions options, [Out] out UInt32 processId);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        public extern IntPtr ActivateForFile([In] String appUserModelId, [In] IntPtr /*IShellItemArray* */ itemArray, [In] String verb, [Out] out UInt32 processId);
+        [MethodImpl(MethodImplOptions.InternalCall, MethodCodeType = MethodCodeType.Runtime)]
+        public extern IntPtr ActivateForProtocol([In] String appUserModelId, [In] IntPtr /* IShellItemArray* */itemArray, [Out] out UInt32 processId);
+    }
+}
---
+++ ToggleExplorer/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToggleExplorer.Enums;
+
+namespace ToggleExplorer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RegistryKey ourKey;
+        public MainWindow()
+        {
+            InitializeComponent();
+            ourKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            ourKey = ourKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon", RegistryKeyPermissionCheck.ReadWriteSubTree);
+            ourKey.SetValue("AutoRestartShell", 0, RegistryValueKind.DWord);
+            ApplicationActivationManager appActiveManager = new ApplicationActivationManager();
+            uint pid;
+            //            Microsoft.SunriseBaseGame_8wekyb3d8bbwe!SunriseReleaseFinal
+            appActiveManager.ActivateApplication("Microsoft.SunriseBaseGame_8wekyb3d8bbwe!SunriseReleaseFinal", null, ActivateOptions.None, out pid);
+            //MessageBox.Show(pid.ToString());
+            ToggleExplorer();
+        }
+
+        private void ToggleExplorerProcess_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleExplorer();
+        }
+
+        private void ToggleExplorer() 
+        {
+            Process[] processes = Process.GetProcessesByName("explorer");
+            if (processes.Count() > 0)
+            {
+                Thread.Sleep(30000);
+                foreach (Process process in processes)
+                {
+                    // process.Close();
+                    process.Kill();
+                }
+            }
+            else
+            {
+                Process.Start($"{System.IO.Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.System))}/explorer.exe");
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            ourKey.SetValue("AutoRestartShell", 1, RegistryValueKind.DWord);
+            Process[] processes = Process.GetProcessesByName("explorer");
+            if (processes.Count() < 1) 
+            { 
+                Process.Start($"{System.IO.Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.System))}/explorer.exe");
+            }
+        }
+    }
+}
